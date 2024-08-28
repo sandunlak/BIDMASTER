64cd462709df724b15872e7b41c8a16ceaf18816
sandunlak <138517951+sandunlak@users.noreply.github.com>
2024-08-28 16:44:49 +0530

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import AddStudent from './components/AddStudent';
+import Header from './components/Header';
+import AllStudents from './components/AllStudents';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+import AddSalesDetails from './components/AddSalesDetails';
+import ReadSalesDetails from './components/ReadSalesDetails';
+import AddProduct from './components/AddProduct';
+import ReadProduct from './components/ReadProduct';
+
+
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes> {/* Wrap Route components inside Routes */}
+          <Route path="/enter" exact element={<AddSalesDetails />} />
+          <Route path="/readproduct" exact element={<ReadProduct/>} />
+          
+          <Route path="/addproduct" exact element={<AddProduct/>} />
+          <Route path="/add" exact element={<AddStudent />} />
+          <Route path="/read" exact element={<ReadSalesDetails/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
